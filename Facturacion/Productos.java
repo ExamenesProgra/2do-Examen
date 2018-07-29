8d9b8434fb6ecb76a0e976c27ac290e136b3e9d4
andrest1998 <39950914+andrest1998@users.noreply.github.com>
2018-07-29 10:18:22 -0600

    Create Productos.java

---
+++ Facturacion/Productos.java
@@ -0,0 +1,40 @@
+
+public class Productos {
+	private int idProducto;
+	private String producto;
+	private int precioProducto;
+	
+  public Productos(int idProducto, String producto, int precioProducto) {
+		this.idProducto = idProducto;
+		this.producto = producto;
+		this.precioProducto = precioProducto;
+	}
+	public Productos() {}
+	
+	public int getIdProducto() {
+		return idProducto;
+	}
+	public void setIdProducto(int idProducto) {
+		this.idProducto = idProducto;
+	}
+	public String getProducto() {
+		return producto;
+	}
+	public void setProducto(String producto) {
+		this.producto = producto;
+	}
+	public int getPrecioProducto() {
+		return precioProducto;
+	}
+	public void setPrecioProducto(int precioProducto) {
+		this.precioProducto = precioProducto;
+	}
+	
+	@Override
+	public String toString() {
+		return "Productos [idProducto=" + idProducto + ", producto=" + producto + ", precioProducto=" + precioProducto
+				+ "]";
+	}
+	
+	
+}
